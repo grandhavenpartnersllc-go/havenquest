2019a29d1f701f09e54bbe8585ba2f9abd31b952
Grand Haven Partners <theguardianscross@gmail.com>
2026-06-30 10:37:04 -0500

    fix: replace video with compressed version under 50MB

--- components/portal/WelcomeVideo.tsx
+++ components/portal/WelcomeVideo.tsx
@@ -14,7 +14,7 @@ const SHARED_STYLE: CSSProperties = {
 export default function WelcomeVideo() {
   return (
     <video
-      src="/videos/navigator-intro.mp4"
+      src="/videos/Navigator-Into-Video-COM.mp4"
       controls
       playsInline
       style={SHARED_STYLE}
